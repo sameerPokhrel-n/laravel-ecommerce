04397266d62ecaeeaa0c867bb49cf23ba78bd78f
sameerPokhrel-n <sameerpokharel29@gmail.com>
2023-08-25 00:02:25 +0545

    feat: test lazygit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,7 +14,7 @@
         <!-- mobile site__header -->
         {{-- @include('includes.mobile-site-header') --}}
         <!-- desktop site__header -->
-        @include('includes.desktop-site-header')
+        {{-- @include('includes.desktop-site-header') --}}
         <!-- site__body -->
         @include('includes.site-body')
         @include('includes.footer')
